63d622704015518af924e78a62ecd752bc382d8f
Mateusz Konieczny <matkoniecz@gmail.com>
2016-10-15 23:25:24 +0200

    less confusing name

--- spec/bicycle_report_spec.rb
+++ spec/bicycle_report_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe ReportGenerator do
 		lat_max = 50.12
 		lon_min = 19.78
 		lon_max = 20.09
-      game = ReportGenerator.new(lat_min, lat_max, lon_min, lon_max)
-      expect(game.start_writing_summary_pages).to eq(nil)
+      generator = ReportGenerator.new(lat_min, lat_max, lon_min, lon_max)
+      expect(generator.start_writing_summary_pages).to eq(nil)
     end
   end
 end 
