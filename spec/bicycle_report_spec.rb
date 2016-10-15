f8644efaf313dd1137d30aae7d7bb6a7e5331310
Mateusz Konieczny <matkoniecz@gmail.com>
2016-10-15 23:33:11 +0200

    tabs to spaces in test

--- spec/bicycle_report_spec.rb
+++ spec/bicycle_report_spec.rb
@@ -3,10 +3,10 @@ require_relative '../lib/bicycle_report'
 RSpec.describe ReportGenerator do
   describe "#start_writing_summary_pages" do
     it "does not crash" do
-		lat_min = 50.00
-		lat_max = 50.12
-		lon_min = 19.78
-		lon_max = 20.09
+      lat_min = 50.00
+      lat_max = 50.12
+      lon_min = 19.78
+      lon_max = 20.09
       generator = ReportGenerator.new(lat_min, lat_max, lon_min, lon_max)
       expect(generator.start_writing_summary_pages).to eq(nil)
     end
