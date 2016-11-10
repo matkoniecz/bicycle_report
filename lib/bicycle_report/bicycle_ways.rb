26ff49b2fec886f410a6ed2076a1af57be15e9e6
Mateusz Konieczny <matkoniecz@gmail.com>
2016-11-10 17:09:54 +0100

    better filenames
    stop hardcoding contraflow
    guard against illegal characters

--- lib/bicycle_report/bicycle_ways.rb
+++ lib/bicycle_report/bicycle_ways.rb
@@ -28,9 +28,24 @@ class BicycleWayRaportGenerator < ReportGenerator
 		return "#{'%.0f' % distance_in_km} km"
 	end
 
+	def title_to_html_filename(title)
+		# TODO - ensure unique titles
+
+		# title is assumed to be safe input, not controlled by an attacker
+
+		# escape characters that would break saving file, as this characters may not be used in filename
+		# there is no better solution according to http://stackoverflow.com/questions/2270635/invalid-chars-filter-for-file-folder-name-ruby
+		title = title.gsub(/[\x00\/\\:\*\?\"<>\|]/, '_')
+
+		# escape characters that would break html (especially <a href="...)
+		# http://stackoverflow.com/questions/7381974/which-characters-need-to-be-escaped-on-html
+		title = title.gsub(/<>&/, '_')
+		return title + ".html"
+	end
+
 	def generate_page_about_ways(json_string, translation_code, color)
 		title = I18n.t(translation_code+'_title')
-		page_filename = title+'.html' #TODO - ensure unique titles
+		page_filename = title_to_html_filename(title)
 		ways = Overhelper.convert_to_ways(json_string)
 		distance_in_m, lines = compute_full_length_and_leafletify_lines(ways, color)
 		open(page_filename, 'w') {|file|
@@ -77,9 +92,9 @@ class BicycleWayRaportGenerator < ReportGenerator
 			file.puts section("contraflow_title", "h2")
 
 			file.puts get_bar_about_contraflow_progress()
-			file.puts get_box_with_way_summary('contraflow_missing_length', @missing_contraflow_in_m, I18n.t('contraflow_missing' + '_title') + '.html')
-			file.puts get_box_with_way_summary('contraflow_existing_length', @existing_contraflow_in_m, I18n.t('contraflow_existing' + '_title') + '.html')
-			file.puts get_box_with_way_summary('contraflow_unwanted_length', @unwanted_contraflow_in_m, I18n.t('contraflow_unwanted' + '_title') + '.html')
+			file.puts get_box_with_way_summary('contraflow_missing_length', @missing_contraflow_in_m, title_to_html_filename(I18n.t('contraflow_missing' + '_title')))
+			file.puts get_box_with_way_summary('contraflow_existing_length', @existing_contraflow_in_m, title_to_html_filename(I18n.t('contraflow_existing' + '_title')))
+			file.puts get_box_with_way_summary('contraflow_unwanted_length', @unwanted_contraflow_in_m, title_to_html_filename(I18n.t('contraflow_unwanted' + '_title')))
 
 			file.puts I18n.t("contraflow_explanation")
 		}
@@ -109,7 +124,7 @@ class BicycleWayRaportGenerator < ReportGenerator
 	end
 
 	def contraflow_generate_general_pages()
-		filename_of_page_with_detailed_data = 'contraflow.html'
+		filename_of_page_with_detailed_data = title_to_html_filename(I18n.t('contraflow_title' + '_title'))
 		generate_general_contraflow_page(filename_of_page_with_detailed_data)
 		add_section_on_main_page_about_contraflow(filename_of_page_with_detailed_data)
 	end
@@ -120,7 +135,7 @@ class BicycleWayRaportGenerator < ReportGenerator
 	end
 
 	def get_box_with_way_summary_simplified(translation_code, length_in_m)
-		get_box_with_way_summary(translation_code+'_length', length_in_m, I18n.t(translation_code + '_title') + '.html')
+		get_box_with_way_summary(translation_code+'_length', length_in_m, title_to_html_filename(I18n.t(translation_code + '_title')))
 	end
 
 	def other_cycling_infrastructure() #TODO - better name needed
@@ -140,10 +155,10 @@ class BicycleWayRaportGenerator < ReportGenerator
 		json_string = get_missing_segregation_status_bicycle_ways_as_json(overpass_bb)
 		generate_page_about_ways(json_string, 'cycling_infrastructure_debug_missing_segregated_key', 'red')
 
-		filename_of_page_with_detailed_data = I18n.t('cycling_infrastructure_title')+'.html'
+		filename_of_page_with_detailed_data = title_to_html_filename(I18n.t('cycling_infrastructure_title'))
 
 		open(main_page, 'a') {|file|
-			file.puts section("cycling_infrastructure_cycleway", "h2")
+			file.puts section("cycling_infrastructure_cycleway_title", "h2")
 			ways = Overhelper.convert_to_ways(cycleway_json_string)
 			length_in_m, _ = compute_full_length_and_leafletify_lines(ways, 'color')
 			file.puts get_box_with_way_summary('cycling_infrastructure_cycleway_length', length_in_m, filename_of_page_with_detailed_data)
